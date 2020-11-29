a02f99543582896b8b5cd0fffe08148b97e3ebf8
b01ix <bekbaev.er@phystech.edu>
2020-11-29 18:34:11 +0100

    version 3

--- src/index.js
+++ src/index.js
@@ -39,6 +39,7 @@ const mintableAmout = document.getElementById('mintableAmount')
 const totalSupply = document.getElementById('totalSupply')
 const deposited = document.getElementById('deposited')
 const tillRetirement = document.getElementById('tillRetirement') 
+const addTokens = document.getElementById('addTokens')
 
 // Permissions Actions Section
 const requestPermissionsButton = document.getElementById('requestPermissions')
@@ -555,6 +556,39 @@ const initialize = async () => {
       }
       mintButton.innerText = "Mint"
       mintButton.disabled = false
+    }
+
+    addTokens.onclick = async() => {
+      const tokenAddress = contractAddress;
+      const tokenSymbol = 'ROLFIE';
+      const tokenDecimals = 2;
+      const tokenImage = currentUrl + '/rcoin.ico';
+
+      console.info(tokenImage)
+
+      try {
+        // wasAdded is a boolean. Like any RPC method, an error may be thrown.
+        const wasAdded = await ethereum.request({
+          method: 'wallet_watchAsset',
+          params: {
+            type: 'ERC20', // Initially only supports ERC20, but eventually more!
+            options: {
+              address: tokenAddress, // The address that the token is at.
+              symbol: tokenSymbol, // A ticker symbol or shorthand, up to 5 chars.
+              decimals: tokenDecimals, // The number of decimals in the token
+              image: tokenImage, // A string url of the token logo
+            },
+          },
+        });
+
+        if (wasAdded) {
+          console.log('Thanks for your interest!');
+        } else {
+          console.log('Your loss!');
+        }
+      } catch (error) {
+        console.log(error);
+      }
     }
   }
 
